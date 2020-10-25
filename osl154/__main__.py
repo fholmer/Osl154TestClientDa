1ed2e77e9079abc4160031899291c3db861f9ad5
Frode Holmer <fholmer@gmail.com>
2020-10-25 11:15:20 +0100

    refactor subparsers

--- osl154/__main__.py
+++ osl154/__main__.py
@@ -2,6 +2,14 @@ import sys
 from . import signdata, opcdata
 from argparse import ArgumentParser
 
+class register():
+    subparsers = []
+    @classmethod
+    def subparser(cls, w):
+        cls.subparsers.append(w)
+        return w
+
+@register.subparser
 def setup_create(subparsers):
     parser = subparsers.add_parser("create", help="Create new sign")
     parser.add_argument("name", type=str, default="SIGN", help="A short name for this sign")
@@ -11,15 +19,28 @@ def setup_create(subparsers):
     parser.add_argument("-height", required=True, type=int, default=64, help="pixel height")
     parser.set_defaults(func=signdata.create_sign_data)
 
+@register.subparser
 def setup_list_servers(subparsers):
     parser = subparsers.add_parser("list-servers", help="Print a list of available OPC servers")
     parser.set_defaults(func=opcdata.list_servers)
 
+@register.subparser
+def setup_list_signs(subparsers):
+    parser = subparsers.add_parser("list-signs", help="Print a list of signs in signs-dir")
+    parser.set_defaults(func=signdata.list_signs)
+
+@register.subparser
 def setup_rgb_on(subparsers):
     parser = subparsers.add_parser("rgb-on", help="Write 1.bmp to opc-server")
     parser.add_argument("name", type=str, default="SIGN", help="A short name for this sign")
     parser.add_argument("-image", type=str, default="1.bmp", help="name of bmp image")
     parser.set_defaults(func=opcdata.rgb_on)
+
+@register.subparser
+def setup_read(subparsers):
+    parser = subparsers.add_parser("read", help="read sign tags from opc-server")
+    parser.add_argument("name", type=str, default="SIGN", help="A short name for this sign")
+    parser.set_defaults(func=opcdata.sign_read)
 
 def main():
     global_parser = ArgumentParser(add_help=True)
@@ -28,7 +49,7 @@ def main():
         title="Commands", description="Additional help for commands: {command} --help"
     )
 
-    for setup in [setup_list_servers, setup_create, setup_rgb_on]:
+    for setup in register.subparsers:
         setup(subparsers)
 
     args = global_parser.parse_args()
